e88cf1d96f393380500da4e7e1242f27fa96bff4
Ataa Allah Ibrahim <Atallah.Mohamed@IbnSina-Pharma.Com>
2024-09-27 17:32:28 +0300

    new seed data

--- Core/Entities/Product.cs
+++ Core/Entities/Product.cs
@@ -11,25 +11,18 @@ namespace Core.Entities
     public class Product :BaseEntity
     {
 
-        [JsonPropertyName("name")]
 
         public required string Name { get; set; }
-        [JsonPropertyName("description")]
 
         public required string Description { get; set; }
-        [JsonPropertyName("price")]
 
         public decimal Price { get; set; }
-        [JsonPropertyName("pictureUrl")]
 
         public required string PictureUrl { get; set; }
-        [JsonPropertyName("type")]
 
         public required string Type { get; set; }
-        [JsonPropertyName("brand")]
-
+       
         public required string Brand { get; set; }
-        [JsonPropertyName("quantityInStock")]
 
         public int QuantityInStock { get; set; }
     }
